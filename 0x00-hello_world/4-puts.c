b637f07da8812e80bc1c985c95e3f1b1fb967ec9
ezinnejulius <ezinnejulius15@gmail.com>
2022-09-11 17:57:24 +0000

    Execute

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - my main function
+ * void: empty parameter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
