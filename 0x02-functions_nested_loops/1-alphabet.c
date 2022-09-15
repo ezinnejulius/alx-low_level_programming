a123cd52f0a7b79e745befd4b556db73c68136f2
ezinnejulius <ezinnejulius15@gmail.com>
2022-09-15 23:29:57 +0000

    Alphabet c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print alphabet - Entry point
+ *
+ * Description: prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char low;
+	for (low = 'a'; low <= 'z'; low++)
+	_putchar(low);
+	_putchar('\n');
+
+	return ();
+}
