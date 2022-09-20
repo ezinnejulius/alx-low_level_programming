93ddf701a252011ceafe5ef324a3a66395d72260
ezinnejulius <ezinnejulius15@gmail.com>
2022-09-20 14:35:30 -0700

    Pointers arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - this resets variable to 98
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
